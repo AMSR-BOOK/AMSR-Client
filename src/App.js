f1a45b63fbbf3f37fe4e9f966567ab128a08698b
SoleiI <kmssun0524@sookmyung.ac.kr>
2024-01-16 17:51:03 +0900

    [UI] App.css 파일 scss로 교체

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
+import styles from './App.module.scss';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <div className={styles.container}>
       <Outlet />
       <Navbar />
-    </>
+    </div>
   );
 }
 
