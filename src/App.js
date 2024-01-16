def20fdbd9cd366d9946b2247526444bcdc6c40d
SoleiI <kmssun0524@sookmyung.ac.kr>
2024-01-16 18:19:48 +0900

    [FEAT] 강조 및 타이틀을 위한 Props 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import styles from './App.module.scss';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
 function App() {
+  const [page, setPage] = useState('');
+  const handleUpdate = (updatedPage) => setPage(updatedPage);
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header page={page} />
       <Outlet />
-      <Navbar />
+      <Navbar page={page} onUpdate={handleUpdate} />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,25 @@ import styles from './Header.module.scss';
 import { IoSettingsSharp } from 'react-icons/io5';
 
 export default function Header({ page }) {
+  const handleTranslate = (pageTitle) => {
+    switch (pageTitle) {
+      case 'search':
+        return '검색';
+      case 'feed':
+        return '피드';
+      case 'mybooks':
+        return '내 책장';
+      case 'setting':
+        return '설정';
+      default:
+        return '검색';
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <h1>{page}</h1>
-      <IoSettingsSharp />
+      <h1 className={styles.title}>{handleTranslate(page)}</h1>
+      <IoSettingsSharp className={styles.setting} />
     </div>
   );
 }
