db6c1cd2b04d429164cb88b1c2a37472ae4ec095
SoleiI <kmssun0524@sookmyung.ac.kr>
2024-01-18 13:23:36 +0900

    [FEAT] 검색창 구현

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SearchResult from '../components/search/SearchResult';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Search() {
-  return <div>Search</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchKey, setSearchKey] = useState('');
+  const key = searchParams.get('key');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchParams({ key: searchKey });
+    console.log(searchParams.get('key'));
+  };
+
+  useEffect(() => setSearchKey(key), [key]);
+
+  return (
+    <section>
+      <header>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="검색어를 입력하세요."
+            value={searchKey}
+            onChange={(e) => setSearchKey(e.target.value)}
+          />
+          <button>검색</button>
+        </form>
+        <button>직접 추가</button>
+      </header>
+      {searchParams.get('key') && <SearchResult param={searchParams} />}
+    </section>
+  );
 }
