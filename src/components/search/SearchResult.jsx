2460c0ec3ef4d3d8dc4f2afc7a4c3661110c993a
SoleiI <kmssun0524@sookmyung.ac.kr>
2024-01-20 11:02:13 +0900

    [UI] 검색 결과 리스트 UI

--- src/components/search/SearchResult.jsx
+++ src/components/search/SearchResult.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import BookCardLarge from '../card/BookCardLarge';
+import styles from './SearchResult.module.scss';
 
 export default function SearchResult({ param }) {
   const [keyword, setKeyword] = useState(param.get('key'));
@@ -19,7 +20,7 @@ export default function SearchResult({ param }) {
       {isLoading && <p>Loading...</p>}
       {error && <p>Error!!!</p>}
       {results && (
-        <ul>
+        <ul className={styles.list}>
           {results.data.map((book) => (
             <BookCardLarge key={book.isbn} book={book} />
           ))}
