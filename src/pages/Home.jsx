7343e9defb2590b168a6422f8b7ea63b12653209
SoleiI <kmssun0524@sookmyung.ac.kr>
2024-01-29 18:04:24 +0900

    [FEAT] 홈 배너 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import styles from './Home.module.scss';
+import { useBookApi } from '../context/BookApiContext';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
-  return <div>See You At Ver.2!!!</div>;
+  const { book } = useBookApi();
+  const {
+    isLoading,
+    error,
+    data: results,
+  } = useQuery({
+    queryKey: ['getReadingBooks'],
+    queryFn: () => book.getReadingBooks(),
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <div className={styles.img}></div>
+        <p className={styles.text}>
+          <span className={styles.title}>아무소록</span>
+          <span className={styles.subtitle}>아무때나 쓰는 작은 독서 기록</span>
+        </p>
+      </div>
+    </div>
+  );
 }
